c4b81388988ef2f4e052630472f3e4249fb168e4
Sunny-zz <1348369672@qq.com>
2019-06-20 17:23:46 +0800

    0620

---
+++ vue-hello/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>hello world</h1>
+    <h2>dksaghd jashg</h2>
+    <Button/>
+  </div>
+</template>
+
+<script>
+import Button from "./components/Button";
+export default {
+  // 小名 不能和 html 的标签名一样 一般都是小写的，一般是必须的
+  name: "app",
+  components: {
+    Button
+  }
+};
+</script>
+
+<style>
+</style>
+
+
---
+++ vue-hello/src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button>按钮</button>
+    <button>按钮</button>
+    <button>按钮</button>
+    <button>按钮</button>
+    <button>按钮</button>
+    <button>按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "btn"
+};
+</script>
+
+<style>
+</style>
