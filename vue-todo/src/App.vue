48abc162155319e45ce1f3e0aad0dca39922aa0d
sunny-zz <b695340402@163.com>
2019-07-01 11:20:31 +0800

    vue-todo

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Todo Mvc</h1>
+    <TodoForm @addTodo="addTodo"/>
+    <Todo @completed="completed" :todos="todos"/>
+  </div>
+</template>
+
+<script>
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+export default {
+  name: "app",
+  components: {
+    Todo,
+    TodoForm
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: "1213",
+          text: "qweqwewqe",
+          isCompleted: true
+        }
+      ]
+    };
+  },
+  methods: {
+    addTodo(text) {
+      const newTodo = {
+        id: new Date().getTime(),
+        text: text,
+        isCompleted: false
+      };
+      this.todos.push(newTodo);
+    },
+    completed(todo) {
+      // 更改数据内 某一个元素的 isCompleted 属性的属性值
+      // 已知某个对象下 id 的属性值   和   一个 数组
+      // find
+      // const todo = this.todo.find(item=> item.id === id)
+      todo.isCompleted = !todo.isCompleted;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-todo/src/components/TodoForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input v-model="val" type="text">
+    <button @click="handleClick">提交</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todoform",
+  data() {
+    return {
+      val: ""
+    };
+  },
+  methods: {
+    handleClick() {
+      if (this.val.trim()) {
+        this.$emit("addTodo", this.val);
+        this.val = "";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
