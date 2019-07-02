d67b13894c519a0452d0626393a532bb538698fd
sunny-zz <b695340402@163.com>
2019-07-02 17:00:39 +0800

    ajax 组件的生命周期

---
+++ vue-juejin/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <img src="https://b-gold-cdn.xitu.io/v3/static/img/logo.a7995ad.svg" alt />
+    </div>
+    <Topics />
+  </div>
+</template>
+
+<script>
+import Topics from "./components/Topics";
+export default {
+  name: "app",
+  components: {
+    Topics
+  }
+};
+</script>
+
+<style>
+</style>
