0933bb348a1e7cd0aff09d4e4e1ce3b2b08bcd0a
sunny-zz <b695340402@163.com>
2019-07-04 17:02:55 +0800

    vue-ui

---
+++ vue-ui/src/components/Posts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    文章列表
+    <!-- 带分页器 -->
+  </div>
+</template>
+
+<script>
+//  homework 分页器  实现分页功能   文章的数据 自己定义
+// [
+//   {
+//     id: "12312",
+//     title: "asdasd"
+//   }
+// ];
+// localhost:3008/posts?_page=5&_limit=10
+export default {};
+</script>
+
+<style>
+</style>
