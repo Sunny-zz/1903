f708fdb1bf330079266c94f5050d26c9583418ca
sunny-zz <b695340402@163.com>
2019-08-05 14:57:09 +0800

    react-todo 拆分组件1

--- react-todo/src/components/TodoForm.js
+++ react-todo/src/components/TodoForm.js
@@ -8,15 +8,26 @@ class TodoForm extends Component {
       text: e.target.value
     })
   }
+  add = e => {
+    // 获取按下的是键盘上哪一个钮
+    // console.log(e.which)
+    // console.log(e.key)
+    // console.log(e.keyCode)
+    if (e.keyCode === 13) {
+      this.props.add(this.state.text)
+    }
+  }
   render() {
+    // 如何绑定键盘事件
     return (
       <div>
         <input
           onChange={this.handleInput}
           type='text'
           value={this.state.text}
+          onKeyDown={this.add}
         />
-        {/* <button>提交</button> */}
+        <button onClick={() => this.props.add(this.state.text)}>提交</button>
       </div>
     )
   }
