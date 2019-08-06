104b2e4c5340df3853afb0f32818a64d472195e3
sunny-zz <b695340402@163.com>
2019-08-06 17:14:10 +0800

    react-router-demo

---
+++ react-router-demo/src/App.js
@@ -0,0 +1,35 @@
+import React from "react"
+import Main from "./components/Main"
+import { NavLink } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
+import "./app.css"
+function App() {
+  return (
+    <Router className='App'>
+      <ul>
+        <li>
+          <NavLink
+            className='nav-item'
+            exact
+            activeStyle={{ color: "red" }}
+            to='/'
+          >
+            home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={{ color: "red" }} to='/about'>
+            about
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={{ color: "red" }} to='/users'>
+            users
+          </NavLink>
+        </li>
+      </ul>
+      <Main />
+    </Router>
+  )
+}
+export default App
---
+++ react-router-demo/src/components/About.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react"
+class About extends Component {
+  render() {
+    return <div>about</div>
+  }
+}
+export default About
